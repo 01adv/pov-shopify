078546929c54afb70f08449b0001456f44d0d7dc
Advaita <148793147+01adv@users.noreply.github.com>
2025-04-28 06:33:11 +0530

    added link slug

--- components/Allworkwear.tsx
+++ components/Allworkwear.tsx
@@ -3,6 +3,7 @@ import Image from "next/image"
 
 import { Badge } from "@/components/ui/badge"
 import SellingLabel from "./SellingLabel"
+import Link from "next/link"
 
 // Product type definition
 type Product = {
@@ -105,9 +106,10 @@ const formatPrice = (price: number) => {
 // Product Card Component
 function ProductCard({ product }: { product: Product }) {
     return (
-        <div className="group relative flex flex-col">
-            <div className="relative aspect-[5/7.2] w-full overflow-hidden bg-slate-100">
-                {/* {product.badge && (
+        <Link href={`/product/${product.slug}`}>
+            <div className="group relative flex flex-col">
+                <div className="relative aspect-[5/7.2] w-full overflow-hidden bg-slate-100">
+                    {/* {product.badge && (
                     product.badge.type === "selling-fast" ? (
                         <SellingLabel label={"Selling Fast"} />
                     ) : (
@@ -120,46 +122,47 @@ function ProductCard({ product }: { product: Product }) {
                         </Badge>
                     )
                 )} */}
-                {product.badge && (
-                    <div className="absolute inset-0 z-10">
-                        {product.badge.type === "selling-fast" ? (
-                            <SellingLabel label={product.badge.text} />
-                        ) : (
-                            <Badge
-                                className={`mt-3 ml-3 bg-slate-100 text-muted-foreground px-3 py-1 tracking-widest rounded-full  
+                    {product.badge && (
+                        <div className="absolute inset-0 z-10">
+                            {product.badge.type === "selling-fast" ? (
+                                <SellingLabel label={product.badge.text} />
+                            ) : (
+                                <Badge
+                                    className={`mt-3 ml-3 bg-slate-100 text-muted-foreground px-3 py-1 tracking-widest rounded-full  
                                     }`}
-                            >
-                                {product.badge.text}
-                            </Badge>
-                        )}
+                                >
+                                    {product.badge.text}
+                                </Badge>
+                            )}
+                        </div>
+                    )}
+                    <Image
+                        src={product.image || "/placeholder.png"}
+                        alt={product.title}
+                        fill
+                        className="object-cover transition-transform duration-300 group-hover:scale-[102%]"
+                        sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
+                    />
+                </div>
+                <div className="py-4 space-y-2">
+                    <h3 className=" text-[13px] text-muted-foreground group-hover:underline ">{product.title}</h3>
+                    <div className=" flex gap-2 items-center ">
+                        {[...Array(5)].map((_, i) => (
+                            <Star key={i} className="h-3 w-3 text-muted-foreground fill-muted-foreground" />
+                        ))}
+                        <span className="ml-1 text-xs text-muted-foreground">({product.reviewCount})</span>
                     </div>
-                )}
-                <Image
-                    src={product.image || "/placeholder.png"}
-                    alt={product.title}
-                    fill
-                    className="object-cover transition-transform duration-300 group-hover:scale-[102%]"
-                    sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
-                />
-            </div>
-            <div className="py-4 space-y-2">
-                <h3 className=" text-[13px] text-muted-foreground group-hover:underline ">{product.title}</h3>
-                <div className=" flex gap-2 items-center ">
-                    {[...Array(5)].map((_, i) => (
-                        <Star key={i} className="h-3 w-3 text-muted-foreground fill-muted-foreground" />
-                    ))}
-                    <span className="ml-1 text-xs text-muted-foreground">({product.reviewCount})</span>
-                </div>
-                {product.discountPercentage && (
-                    <Badge className=" w-fit bg-primary text-white py-2 px-7">{product.discountPercentage}% OFF</Badge>
-                )}
-                <div className="mt-auto flex flex-col md:flex-row items-baseline gap-2  lg:gap-3">
-                    <span className="text-sm text-slate-500 line-through">{formatPrice(product.originalPrice)}</span>
-                    <span className="text-lg text-muted-foreground">{formatPrice(product.price)}</span>
-                </div>
+                    {product.discountPercentage && (
+                        <Badge className=" w-fit bg-primary text-white py-2 px-7">{product.discountPercentage}% OFF</Badge>
+                    )}
+                    <div className="mt-auto flex flex-col md:flex-row items-baseline gap-2  lg:gap-3">
+                        <span className="text-sm text-slate-500 line-through">{formatPrice(product.originalPrice)}</span>
+                        <span className="text-lg text-muted-foreground">{formatPrice(product.price)}</span>
+                    </div>
 
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 

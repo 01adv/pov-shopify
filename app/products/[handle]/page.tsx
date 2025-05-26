86d108867ec5d5cceededb4a26be302e5ab72a84
Advaita <148793147+01adv@users.noreply.github.com>
2025-05-26 10:44:41 +0530

    Update page.tsx

--- app/products/[handle]/page.tsx
+++ app/products/[handle]/page.tsx
@@ -253,7 +253,7 @@ export default async function ProductPage({ params, searchParams }: ProductPageP
                                 >
                                     {selectedVariant.available ? "Add to Cart" : "Out of Stock"}
                                 </Button> */}
-                                <AddToCartButton variantId={44482757066805} />
+                                <AddToCartButton variantId={8839600603348} />
                             </div>
                             <span
                                 id="product-details"

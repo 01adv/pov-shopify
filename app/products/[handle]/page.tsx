eff20636dd027d659db8e180ed9f148e2a5ea154
Advaita <148793147+01adv@users.noreply.github.com>
2025-06-12 02:14:23 +0530

    sold out button skipped for later

--- app/products/[handle]/page.tsx
+++ app/products/[handle]/page.tsx
@@ -176,19 +176,29 @@ export default async function ProductPage({ params, searchParams }: ProductPageP
                                     <span className="text-lg text-muted-foreground">
                                         $ {product.salePrice.toFixed(2)}
                                     </span>
-                                    {product.availableSizes.some(size => !size.available) ? (
-                                        <Badge className="text-white text-xs px-3 py-0.5 bg-secondary outline rounded-full">
-                                            Sold Out
-                                        </Badge>
-                                    ) : productData.tags.includes("Bestseller") ? (
-                                        <Badge className="text-black text-xs px-3 py-0.5 bg-white outline rounded-full">
-                                            Bestseller
-                                        </Badge>
-                                    ) : product.originalPrice ? (
-                                        <Badge className="text-black text-xs px-3 py-0.5 bg-white outline rounded-full">
-                                            Sale
-                                        </Badge>
-                                    ) : null}
+                                    {
+                                        // product.availableSizes
+                                        //     .filter((size) => {
+                                        //         const variant = productData.variants.find(
+                                        //             (v) => v.option1 === product.color && v.option2 === size.size
+                                        //         );
+                                        //         return variant !== undefined; // Only consider sizes relevant to the selected color
+                                        //     })
+                                        //     .every((size) => !size.available) ? (
+                                        //     <Badge className="text-white text-xs px-3 py-0.5 bg-secondary outline rounded-full">
+                                        //         Sold Out
+                                        //     </Badge>
+                                        // ) : 
+                                        productData.tags.includes("Bestseller") ? (
+                                            <Badge className="text-black text-xs px-3 py-0.5 bg-white outline rounded-full">
+                                                Bestseller
+                                            </Badge>
+                                        ) : product.originalPrice ? (
+                                            <Badge className="text-black text-xs px-3 py-0.5 bg-white outline rounded-full">
+                                                Sale
+                                            </Badge>
+                                        ) : null
+                                    }
                                 </div>
                                 {/* Color */}
                                 <div className="space-y-1 flex flex-col">

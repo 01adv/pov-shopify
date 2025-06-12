e04f7a64cd1b37b891dd69ba07a8eb3326f5546f
Advaita <148793147+01adv@users.noreply.github.com>
2025-06-13 02:18:28 +0530

    new loading ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
 'use client';
 
 import { logEvent } from '@/lib/logger';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+
+const Headings = [
+    "There’s a lot here—I can curate a look that matches your mood?",
+    "Scrolling strains the thumb (ouch!) — instead say ‘want a posh look under $150”",
+    "You could browse. Or you could say: ‘Black, versatile, not boring dress’",
+]
 
 const Page = () => {
     const [pageName, setPageName] = useState('unknown-page');
     const [fullPath, setFullPath] = useState('');
     const [isLoading, setIsLoading] = useState(true);
+    const [currentHeading, setCurrentHeading] = useState(Headings[0]);
     const router = useRouter();
+
+
+    // Cycle heading every 1 second
+    useEffect(() => {
+        if (!isLoading) return;
+
+        const interval = setInterval(() => {
+            const randomIndex = Math.floor(Math.random() * Headings.length);
+            setCurrentHeading(Headings[randomIndex]);
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, [isLoading]);
 
     // Embedded hook logic to listen for messages
     useEffect(() => {
@@ -55,9 +76,13 @@ const Page = () => {
     // Optional loading state
     if (isLoading) {
         return (
-            <div>
-                <h1>Chatbot Iframe</h1>
-                <p>Loading...</p>
+            // <div className='absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-sm:w-full px-4'>
+            <div className='flex max-w-4xl mx-auto  flex-col min-h-[calc(100vh-200px)]  items-center justify-center md:justify-end max-sm:w-full px-4 '>
+                <h1 className='text-xl md:text-3xl text-center max-sm:pt-10'>{currentHeading}</h1>
+                <div className='flex justify-center pt-14'>
+                    <Image src="/downArrow.svg" alt="Arrow" width={80} height={80} className='animate-bounce' />
+                </div>
+                <p className='hidden md:block text-center text-lg pt-10'>Just type here, and I&apos;ll do the digging.</p>
             </div>
         );
     }
@@ -65,7 +90,7 @@ const Page = () => {
     // Fallback content
     return (
         <div>
-            <h1>Chatbot Iframe</h1>
+            <h1>There&apos;s a lot here—I can curate a look that matches your mood?</h1>
             <p>Current Page: {pageName}</p>
         </div>
     );

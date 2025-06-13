cac8524d2ab1a3776b5f6053bfd5bbb0c41b4ed6
Advaita <148793147+01adv@users.noreply.github.com>
2025-06-13 22:41:44 +0530

    loading

--- app/page.tsx
+++ app/page.tsx
@@ -89,11 +89,16 @@ const Page = () => {
 
     // Fallback content
     return (
-        <div>
-            <h1>There&apos;s a lot here—I can curate a look that matches your mood?</h1>
-            <p>Current Page: {pageName}</p>
+        // <div className='absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-sm:w-full px-4'>
+        <div className='flex max-w-4xl mx-auto  flex-col min-h-[calc(100vh-200px)]  items-center justify-center md:justify-end max-sm:w-full px-4 '>
+            <h1 className='text-xl md:text-3xl text-center max-sm:pt-10'>{currentHeading}</h1>
+            <div className='flex justify-center pt-14'>
+                <Image src="/downArrow.svg" alt="Arrow" width={80} height={80} className='animate-bounce' />
+            </div>
+            <p className='hidden md:block text-center text-lg pt-10'>Just type here, and I&apos;ll do the digging.</p>
         </div>
     );
+
 };
 
 export default Page;
